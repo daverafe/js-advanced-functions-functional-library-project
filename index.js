8c1455568b766d619fdc5c69013291aaed23b6b1
David Rafe <david.rafe@gmail.com>
2021-06-16 14:42:44 -0400

    commit tree 7694ea1a92573b7ef29577dc697e46bd74be8eb9

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+     if(Array.isArray(collection)) {
+       collection.forEach(element => {
+         return callback(element, index, collection)
+       })
+       return collection 
+     } else {
+       for (const key in collection) {
+         return function callback(value, key, collection) {
+           alert(key)
+         }
+       }
+       return collection 
+     }
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+     return function callback(value, index, collection) {
+        let newArray = collection.map()
+     }
     },
 
     reduce: function() {
@@ -19,6 +33,10 @@ const fi = (function() {
     functions: function() {
 
     },
+
+    first: function(array, n) {
+      return array[0]
+    }
 
 
   }
